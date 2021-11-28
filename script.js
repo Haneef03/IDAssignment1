478f20f6871f9f95ae2c81cba99c729d11504334
Your own username <Your own email>
2021-11-28 14:11:26 +0800

    implemented top down nav bar for parts and acc

--- script.js
+++ script.js
@@ -16,4 +16,14 @@ function myFunction() {
   }
   function exploreLudicrous() {
     window.location.href="ludicrousBuild.html";
-  }  
+  }
+  
+  window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+}
